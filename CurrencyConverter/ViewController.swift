dd5fcbda9fcc619c1ccf0513209c5c68a1a7f890
the-hyperborean <jtb@abaris.com.au>
2020-12-08 16:22:31 +1100

    Lecture 1

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test")
+        print("test2")
     }
 
-
+    @IBAction func getRatesClicked(_ sender: Any) {
+    }
+    
 }
 
